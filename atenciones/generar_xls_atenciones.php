2c2f09193b4216d1eb7205f7995b99b74b0e42ef
Luis Salazar <luis_s23171@hotmail.com>
2017-09-22 15:58:06 -0430

    se agrega nueva hoja comunidades atendidas por rango de fecha

--- atenciones/generar_xls_atenciones.php
+++ atenciones/generar_xls_atenciones.php
@@ -5,8 +5,13 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 include_once("xlsxwriter.class.php");
 
+include '../spoon/spoon.php';
+$objDB= new DBConexion();
+$fechaInicial = $_POST['fechainicial'];
+$fechaFinal = $_POST['fechafinal'];
 
-$filename = "atenciones.xlsx";
+$filename = "Atenciones - desde: " . $fechaInicial . " hasta: " . $fechaFinal . ".xlsx";
+
 header('Content-disposition: attachment; filename="'.XLSXWriter::sanitize_filename($filename).'"');
 header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 header('Content-Transfer-Encoding: binary');
@@ -20,10 +25,7 @@ $headerResumen = array(
 );
 
 
- include '../spoon/spoon.php';
-        $objDB= new DBConexion();
- $fechaInicial = $_POST['fechainicial'];
- $fechaFinal = $_POST['fechafinal'];
+
 
 $fecha = date_create_from_format("d/m/Y", $fechaInicial) ;                        
 $fechaInicial = date_format($fecha, "Y-m-d");
@@ -36,7 +38,7 @@ $header = array(
     'Items' => 'string',
     'Nombres y Apellidos' => 'string',
     'Cédula' => 'string',
-    'Ente u Organismo' => 'string',
+    'Comunidad u Organismo' => 'string',
     'Municipio' => 'string',
     'Parroquia' => 'string',
     'Sector' => 'string',
@@ -46,24 +48,78 @@ $header = array(
     'Fecha'=>'date',
 );
 
-$query = "SELECT @curRow := @curRow +1 AS items,
-                 CONCAT(nombres, ' ' , apellidos) AS nombre_completo, 
-                 cedula, 
-                 comunidades.comunidad AS ente, 
-                 municipio, 
-                 parroquia, 
-                 comunidades.comunidad AS community, 
-                 narracion_hechos, 
-                 observaciones,  
-                 telefonos, 
-                 atenciones.fecha_registro as fechareg 
-        FROM atenciones
-        INNER JOIN comunidades ON comunidades.id_comunidad = atenciones.comunidad
-        LEFT JOIN  parroquias_barinas ON parroquias_barinas.id = comunidades.id_parroquia
-        INNER JOIN ciudadanos ON ciudadanos.id_ciudadano = atenciones.id_ciudadano, 
-        (SELECT @curRow :=0 ) r
-        WHERE atenciones.fecha_registro >=  '{$fechaInicial}'
-        AND atenciones.fecha_registro <=  '{$fechaFinal}'";
+if (strtotime($fechaInicial)<=strtotime("2017-09-04")){
+
+
+    $header = array(
+        'Items' => 'string',
+        'Nombres y Apellidos' => 'string',
+        'Cédula' => 'string',
+        'Comunidad u Organismo' => 'string',
+        'Municipio' => 'string',
+        'Parroquia' => 'string',
+        'Sector' => 'string',
+        'Hechos'=>'string',
+        'Observaciones'=>'string',
+        'Teléfono' => 'string',
+        'Fecha'=>'date',
+    );
+
+    $query = "SELECT @curRow := @curRow +1 AS items,
+                    CONCAT(nombres, ' ' , apellidos) AS nombre_completo, 
+                    cedula, 
+                    comunidades.comunidad AS ente, 
+                    municipio, 
+                    parroquia, 
+                    comunidades.comunidad AS community, 
+                    narracion_hechos, 
+                    observaciones,  
+                    telefonos, 
+                    atenciones.fecha_registro as fechareg 
+            FROM atenciones
+            INNER JOIN comunidades ON comunidades.id_comunidad = atenciones.comunidad
+            LEFT JOIN  parroquias_barinas ON parroquias_barinas.id = comunidades.id_parroquia
+            INNER JOIN ciudadanos ON ciudadanos.id_ciudadano = atenciones.id_ciudadano, 
+            (SELECT @curRow :=0 ) r
+            WHERE atenciones.fecha_registro >=  '{$fechaInicial}'
+            AND atenciones.fecha_registro <=  '{$fechaFinal}'";
+}else{
+
+
+    $header = array(
+        'Items' => 'string',
+        'Nombres y Apellidos' => 'string',
+        'Cédula' => 'string',
+        'Estado' => 'string',
+        'Municipio' => 'string',
+        'Parroquia' => 'string',
+        'Sector' => 'string',
+        'Hechos'=>'string',
+        'Observaciones'=>'string',
+        'Teléfono' => 'string',
+        'Fecha'=>'date',
+    );
+    $query = "SELECT @curRow := @curRow +1 AS items,
+                    CONCAT(nombres, ' ' , apellidos) AS nombre_completo, 
+                    cedula, 
+                    estado,
+                    municipio, 
+                    parroquia, 
+                    comunidades.comunidad AS community, 
+                    narracion_hechos, 
+                    observaciones,  
+                    telefonos, 
+                    atenciones.fecha_registro as fechareg 
+                FROM atenciones
+                INNER JOIN comunidades ON comunidades.id_comunidad = atenciones.comunidad
+                INNER JOIN  parroquias ON parroquias.id_parroquia = comunidades.id_parroquia
+                INNER JOIN  municipios ON municipios.id_municipio = parroquias.id_municipio                
+                INNER JOIN  estados ON estados.id_estado = municipios.id_estado  
+                INNER JOIN ciudadanos ON ciudadanos.id_ciudadano = atenciones.id_ciudadano, 
+                (SELECT @curRow :=0 ) r
+                WHERE atenciones.fecha_registro >=  '{$fechaInicial}'
+                AND atenciones.fecha_registro <=  '{$fechaFinal}'";
+}
         //, ciudadanos.direccion as address
         //LEFT JOIN ciudadanos ON ciudadanos.id_ciudadano = atenciones.id_ciudadano
 
@@ -75,7 +131,8 @@ FROM atenciones
 INNER JOIN comunidades ON comunidades.id_comunidad = atenciones.comunidad
 WHERE atenciones.fecha_registro >= '{$fechaInicial}'
 AND atenciones.fecha_registro <= '{$fechaFinal}' GROUP BY narracion_hechos ORDER BY cantidad";
-                
+
+$objDB->execute("SET NAMES utf8");                
 $atencionesArray = $objDB->getRecords($query);
 
 $atencionesCountArray = $objDB->getRecords($queryCount);
@@ -87,9 +144,34 @@ $atencionesCountArray = $objDB->getRecords($queryCount);
 //    array('2003','01','343.12'),
 //    array('2003','02','345.12'),
 //);
+
+//************************************************************** Lista de Comunidades atendidas por rango de fecha
+$headerComunidades = array(
+    'Comunidad u Organismo' => 'string'
+);
+
+$queryComunidades="SELECT 
+            DISTINCT(comunidades.comunidad) 
+            FROM atenciones
+            INNER JOIN comunidades ON atenciones.comunidad = comunidades.id_comunidad
+            WHERE atenciones.fecha_registro
+            BETWEEN  '{$fechaInicial}'
+            AND  '{$fechaFinal}'
+            ORDER BY comunidades.comunidad ASC";
+
+$objDB->execute("SET NAMES utf8");                
+$comunidadesArray = $objDB->getRecords($queryComunidades);
+
+
+//************************************************************** Fin Lista de Comunidades atendidas por rango de fecha
+
+
+
 $writer = new XLSXWriter();
-$writer->setAuthor('Some Author');
+$writer->setAuthor('Sistema de Atención al Ciudadano - CEB');
 $writer->writeSheet($atencionesArray,'ListadoDeAtenciones',$header);
+$writer->writeSheet($comunidadesArray,'ListadoDeComunidades',$headerComunidades);
+
 $writer->writeSheet($atencionesCountArray,'AtencionesTotalizadas',$headerResumen);
 //$writer->writeSheet($data2,'Sheet2');
 $writer->writeToStdOut();
